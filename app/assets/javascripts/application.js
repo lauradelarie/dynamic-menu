34d1cde8e99e4c1b16c46dd91ff881db5eb6ef0f
MalenkavanOmmen <malenkavanommen@gmail.com>
2016-11-23 10:57:42 +0100

    Add dropdown (not functional yet)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -317,3 +317,12 @@ $(function () {
   $('[data-toggle="popover"]').popover();
   container: 'body'
 })
+
+$('#tablelist li').on('click', function(){
+    $('#datebox').val($(this).text());
+});
+
+$(".dropdown").on("show.bs.dropdown", function(event){
+    var x = $(event.relatedTarget).text(); // Get the text of the element
+    alert(x);
+});
