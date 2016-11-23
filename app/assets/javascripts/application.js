b9e48af5ca75a5ac40be97bf9b1017a4a4266af0
MalenkavanOmmen <malenkavanommen@gmail.com>
2016-11-23 15:32:40 +0100

    No JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,313 +16,313 @@
 //= require turbolinks
 //= require_tree .
 
-var choicesArray = [];
-
-function makeDescription(){
-  var description = choicesArray.join(', ');
-  $('ul.description').append('<li></li>').html(description);
-}
-
-// Order funtions//
-///////////////////////////////////////////////////////////////////////////////////
-
-function createOrder(price, choice, tableNr){
-
-  $.ajax({
-    type: "POST",
-    url: "/hamburgers",
-    data: JSON.stringify({
-      order: {
-      total_price: price,
-      choise: choice,
-      table: tableNr,
-    }
-  }),
-
-    contentType: "application/json",
-    dataType: "json"
-  })
-
-  .success(function(data) {
-    var succes = $('<li></li>').html("Sent to the kitchen!");
-    $('ul.description').append(succes);
-  })
-
-  .fail(function(error) {
-    errors = JSON.parse(error.responseText).error
-
-    $.each(errors, function(index, value) {
-      var errorItem = $("<li></li>").html(value);
-      $("#errors").append(errorItem);
-    });
-  });
-
-}
-
-function submitOrder(event) {
-  var price = Number($(event.target).siblings('.total-price').html());
-  var choice = $(event.target).siblings('.description').html();
-  var tableNr = $('.dropdownmenu').val();
-
-  createOrder(price, choice, tableNr);
-}
-
-
-var pricesArray = [];
-
-function calculateTotal(){
-  var totalPrice;
-  (pricesArray.length > 0) ?
-  totalPrice = pricesArray.reduce((a, b) => {
-    return a + b
-  }).toFixed(2) : (totalPrice = 0);
-
-  $('ul.total-price').append('<li>€</li>').html(totalPrice);
-}
-
-// Sauce funtions//
-///////////////////////////////////////////////////////////////////////////////////
-
-function removeSauce(event) {
-//remove side from orderlist
-  var sauceId = $(event.target).parent().siblings('.sauce-name').attr('id');
-  $('#sauce-' + sauceId).remove();
-//remove sides price from pricesArray
-  var saucePrice = Number($(event.target).parent().siblings('.sauce-name').data("price").Price);
-  var index = pricesArray.indexOf(saucePrice);
-  pricesArray.splice(index, 1);
-
-  calculateTotal();
-
-//remove the sides name from the choicesArray
-  var sauceName = $(event.target).parent().siblings('.sauce-name').html();
-  var sauceIndex = choicesArray.indexOf(sauceName);
-  choicesArray.splice(sauceIndex, 1);
-
-  makeDescription();
-}
-
-function addSauce(event) {
-  var sauce = $(event.target).parent().siblings('.sauce-name').html();
-  var sauceId = $(event.target).parent().siblings('.sauce-name').attr('id');
-  var newSauce = $('<li></li>').html(sauce);
-  newSauce.attr('id', 'sauce-' + sauceId);
-  newSauce.attr('val', 1);
-
-  $('#order-list-sauces').append(newSauce);
-
-  // add the topping's price to the pricesArray
-  var saucePrice = Number($(event.target).parent().siblings('.sauce-name').data("price").Price);
-
-  pricesArray.push(saucePrice);
-  calculateTotal();
-
-  choicesArray.push(sauce);
-  makeDescription();
-}
-
-
-// Side funtions//
-///////////////////////////////////////////////////////////////////////////////////
-
-function removeSide(event) {
-//remove side from orderlist
-  var sideId = $(event.target).parent().siblings('.side-name').attr('id');
-  $('#side-' + sideId).remove();
-//remove sides price from pricesArray
-  var sidePrice = Number($(event.target).parent().siblings('.side-name').data("price").Price);
-  var index = pricesArray.indexOf(sidePrice);
-  pricesArray.splice(index, 1);
-
-  calculateTotal();
-
-//remove the sides name from the choicesArray
-  var sideName = $(event.target).parent().siblings('.side-name').html();
-  var sideIndex = choicesArray.indexOf(sideName);
-  choicesArray.splice(sideIndex, 1);
-
-  makeDescription();
-}
-
-function addSide(event) {
-  var side = $(event.target).parent().siblings('.side-name').html();
-  var sideId = $(event.target).parent().siblings('.side-name').attr('id');
-  var newSide = $('<li></li>').html(side);
-  newSide.attr('id', 'side-' + sideId);
-  newSide.attr('val', 1);
-
-  $('#order-list-sides').append(newSide);
-
-  // add the topping's price to the pricesArray
-  var sidePrice = Number($(event.target).parent().siblings('.side-name').data("price").Price);
-
-  pricesArray.push(sidePrice);
-  calculateTotal();
-
-  choicesArray.push(side);
-  makeDescription();
-}
-
-
-//Topping Functions//
-///////////////////////////////////////////////////////////////////////////////////
-
-function removeTopping(event) {
-//remove topping from orderlist
-  var toppingId = $(event.target).parent().siblings('.topping-name').attr('id');
-  $('#topping-' + toppingId).remove();
-//remove topping's price from pricesArray
-  var toppingPrice = Number($(event.target).parent().siblings('.topping-name').data("price").Price);
-  var index = pricesArray.indexOf(toppingPrice);
-  pricesArray.splice(index, 1);
-
-  calculateTotal();
-
-//remove the topping's name from the choicesArray
-  var toppingName = $(event.target).parent().siblings('.topping-name').html();
-  var toppingIndex = choicesArray.indexOf(toppingName);
-  choicesArray.splice(toppingIndex, 1);
-
-  makeDescription();
-}
-
-function addTopping(event) {
-  var topping = $(event.target).parent().siblings('.topping-name').html();
-  var toppingId = $(event.target).parent().siblings('.topping-name').attr('id');
-  var newTopping = $('<li></li>').html(topping);
-  newTopping.attr('id', 'topping-' + toppingId);
-  newTopping.attr('val', 1);
-
-  $('#order-list-toppings').append(newTopping);
-
-  // add the topping's price to the pricesArray
-  var toppingPrice = Number($(event.target).parent().siblings('.topping-name').data("price").Price);
-
-  pricesArray.push(toppingPrice);
-  calculateTotal();
-
-  choicesArray.push(topping);
-  makeDescription();
-}
-
-
-// Burger functions//
-///////////////////////////////////////////////////////////////////////////////////
-
-function removeBurger(event){
-
-//remove the burger from the orderlist
-  var burgerId = $(event.target).parent().attr('id');
-  $('#' + burgerId).remove();
-
-//remove the burger's price from the pricesArray
-  var burgerPrice = Number($(event.target).parent().attr('data-price'));
-  var priceIndex = pricesArray.indexOf(burgerPrice);
-  pricesArray.splice(priceIndex, 1);
-
-  calculateTotal();
-
-  //remove the burger's name from the choicesArray
-  var burgerName = $(event.target).siblings('.meat-name').html();
-  var choiceIndex = choicesArray.indexOf(burgerName);
-  choicesArray.splice(choiceIndex, 1);
-
-  makeDescription();
-
-  $('.choose-burger-button').show();
-}
-
-function addBurger(event) {
-  event.preventDefault();
-
-  var burgerPrice = Number($(event.target).parent().siblings('.burger-price').data("price").Price);
-  var parent = $(event.target).parent().siblings('.burger-name');
-  var meat = parent.html();
-  var burgerId = parent.attr('id');
-  var newBurger = $('<li></li>').html('<div class="meat-name">' + meat + '</div>');
-  newBurger.attr('id', 'burger-' + burgerId);
-  newBurger.attr('data-price', burgerPrice);
-
-  $('#order-list-burger').append(newBurger);
-
-  var deleteButton = $('<input>').html('X');
-  deleteButton.attr('type', 'button');
-  deleteButton.attr('onClick', 'removeBurger(event)');
-
-  newBurger.append(deleteButton);
-
-  $('.choose-burger-button').hide();
-
-  pricesArray.push(burgerPrice);
-  calculateTotal();
-
-  choicesArray.push(meat);
-  makeDescription();
-}
-
-//choose a table dropdown
-///////////////////////////////////////////////////////////////////////////////////
-
-function chooseTable(){
-  var tableNr = $('.dropdownmenu').val();
-  $('#table-number').append(tableNr);
-}
-
-///////////////////////////////////////////////////////////////////////////////////
-$(document).on('turbolinks:load', function(){
-  $('.choose-burger-button').bind('click', addBurger);
-
-  $('.dropdownmenu').bind('change', chooseTable);
-
-  var check;
-  $('.choose-topping-checkbox').bind('click', function(){
-    check = $(event.target).prop("checked");
-
-    if(check) {
-      console.log("checked");
-      addTopping(event);
-    } else {
-      console.log("not checked");
-      removeTopping(event);
-    };
-
-  });
-
-  var check;
-  $('.choose-side-checkbox').bind('click', function(){
-    check = $(event.target).prop("checked");
-
-    if(check) {
-      console.log("checked");
-      addSide(event);
-    } else {
-      console.log("not checked");
-      removeSide(event);
-    };
-
-  });
-
-  var check;
-  $('.choose-sauce-checkbox').bind('click', function(){
-    check = $(event.target).prop("checked");
-
-    if(check) {
-      console.log("checked");
-      addSauce(event);
-    } else {
-      console.log("not checked");
-      removeSauce(event);
-    };
-
-  });
-
-  $('.place-order').bind('click', submitOrder);
-
-  $(function() {
-    $('[data-toggle="popover"]').popover();
-    container: 'body'
-  });
-});
-
-//popover
-///////////////////////////////////////////////////////////////////////////////////
+// var choicesArray = [];
+//
+// function makeDescription(){
+//   var description = choicesArray.join(', ');
+//   $('ul.description').append('<li></li>').html(description);
+// }
+//
+// // Order funtions//
+// ///////////////////////////////////////////////////////////////////////////////////
+//
+// function createOrder(price, choice, tableNr){
+//
+//   $.ajax({
+//     type: "POST",
+//     url: "/hamburgers",
+//     data: JSON.stringify({
+//       order: {
+//       total_price: price,
+//       choise: choice,
+//       table: tableNr,
+//     }
+//   }),
+//
+//     contentType: "application/json",
+//     dataType: "json"
+//   })
+//
+//   .success(function(data) {
+//     var succes = $('<li></li>').html("Sent to the kitchen!");
+//     $('ul.description').append(succes);
+//   })
+//
+//   .fail(function(error) {
+//     errors = JSON.parse(error.responseText).error
+//
+//     $.each(errors, function(index, value) {
+//       var errorItem = $("<li></li>").html(value);
+//       $("#errors").append(errorItem);
+//     });
+//   });
+//
+// }
+//
+// function submitOrder(event) {
+//   var price = Number($(event.target).siblings('.total-price').html());
+//   var choice = $(event.target).siblings('.description').html();
+//   var tableNr = $('.dropdownmenu').val();
+//
+//   createOrder(price, choice, tableNr);
+// }
+//
+//
+// var pricesArray = [];
+//
+// function calculateTotal(){
+//   var totalPrice;
+//   (pricesArray.length > 0) ?
+//   totalPrice = pricesArray.reduce((a, b) => {
+//     return a + b
+//   }).toFixed(2) : (totalPrice = 0);
+//
+//   $('ul.total-price').append('<li>€</li>').html(totalPrice);
+// }
+//
+// // Sauce funtions//
+// ///////////////////////////////////////////////////////////////////////////////////
+//
+// function removeSauce(event) {
+// //remove side from orderlist
+//   var sauceId = $(event.target).parent().siblings('.sauce-name').attr('id');
+//   $('#sauce-' + sauceId).remove();
+// //remove sides price from pricesArray
+//   var saucePrice = Number($(event.target).parent().siblings('.sauce-name').data("price").Price);
+//   var index = pricesArray.indexOf(saucePrice);
+//   pricesArray.splice(index, 1);
+//
+//   calculateTotal();
+//
+// //remove the sides name from the choicesArray
+//   var sauceName = $(event.target).parent().siblings('.sauce-name').html();
+//   var sauceIndex = choicesArray.indexOf(sauceName);
+//   choicesArray.splice(sauceIndex, 1);
+//
+//   makeDescription();
+// }
+//
+// function addSauce(event) {
+//   var sauce = $(event.target).parent().siblings('.sauce-name').html();
+//   var sauceId = $(event.target).parent().siblings('.sauce-name').attr('id');
+//   var newSauce = $('<li></li>').html(sauce);
+//   newSauce.attr('id', 'sauce-' + sauceId);
+//   newSauce.attr('val', 1);
+//
+//   $('#order-list-sauces').append(newSauce);
+//
+//   // add the topping's price to the pricesArray
+//   var saucePrice = Number($(event.target).parent().siblings('.sauce-name').data("price").Price);
+//
+//   pricesArray.push(saucePrice);
+//   calculateTotal();
+//
+//   choicesArray.push(sauce);
+//   makeDescription();
+// }
+//
+//
+// // Side funtions//
+// ///////////////////////////////////////////////////////////////////////////////////
+//
+// function removeSide(event) {
+// //remove side from orderlist
+//   var sideId = $(event.target).parent().siblings('.side-name').attr('id');
+//   $('#side-' + sideId).remove();
+// //remove sides price from pricesArray
+//   var sidePrice = Number($(event.target).parent().siblings('.side-name').data("price").Price);
+//   var index = pricesArray.indexOf(sidePrice);
+//   pricesArray.splice(index, 1);
+//
+//   calculateTotal();
+//
+// //remove the sides name from the choicesArray
+//   var sideName = $(event.target).parent().siblings('.side-name').html();
+//   var sideIndex = choicesArray.indexOf(sideName);
+//   choicesArray.splice(sideIndex, 1);
+//
+//   makeDescription();
+// }
+//
+// function addSide(event) {
+//   var side = $(event.target).parent().siblings('.side-name').html();
+//   var sideId = $(event.target).parent().siblings('.side-name').attr('id');
+//   var newSide = $('<li></li>').html(side);
+//   newSide.attr('id', 'side-' + sideId);
+//   newSide.attr('val', 1);
+//
+//   $('#order-list-sides').append(newSide);
+//
+//   // add the topping's price to the pricesArray
+//   var sidePrice = Number($(event.target).parent().siblings('.side-name').data("price").Price);
+//
+//   pricesArray.push(sidePrice);
+//   calculateTotal();
+//
+//   choicesArray.push(side);
+//   makeDescription();
+// }
+//
+//
+// //Topping Functions//
+// ///////////////////////////////////////////////////////////////////////////////////
+//
+// function removeTopping(event) {
+// //remove topping from orderlist
+//   var toppingId = $(event.target).parent().siblings('.topping-name').attr('id');
+//   $('#topping-' + toppingId).remove();
+// //remove topping's price from pricesArray
+//   var toppingPrice = Number($(event.target).parent().siblings('.topping-name').data("price").Price);
+//   var index = pricesArray.indexOf(toppingPrice);
+//   pricesArray.splice(index, 1);
+//
+//   calculateTotal();
+//
+// //remove the topping's name from the choicesArray
+//   var toppingName = $(event.target).parent().siblings('.topping-name').html();
+//   var toppingIndex = choicesArray.indexOf(toppingName);
+//   choicesArray.splice(toppingIndex, 1);
+//
+//   makeDescription();
+// }
+//
+// function addTopping(event) {
+//   var topping = $(event.target).parent().siblings('.topping-name').html();
+//   var toppingId = $(event.target).parent().siblings('.topping-name').attr('id');
+//   var newTopping = $('<li></li>').html(topping);
+//   newTopping.attr('id', 'topping-' + toppingId);
+//   newTopping.attr('val', 1);
+//
+//   $('#order-list-toppings').append(newTopping);
+//
+//   // add the topping's price to the pricesArray
+//   var toppingPrice = Number($(event.target).parent().siblings('.topping-name').data("price").Price);
+//
+//   pricesArray.push(toppingPrice);
+//   calculateTotal();
+//
+//   choicesArray.push(topping);
+//   makeDescription();
+// }
+//
+//
+// // Burger functions//
+// ///////////////////////////////////////////////////////////////////////////////////
+//
+// function removeBurger(event){
+//
+// //remove the burger from the orderlist
+//   var burgerId = $(event.target).parent().attr('id');
+//   $('#' + burgerId).remove();
+//
+// //remove the burger's price from the pricesArray
+//   var burgerPrice = Number($(event.target).parent().attr('data-price'));
+//   var priceIndex = pricesArray.indexOf(burgerPrice);
+//   pricesArray.splice(priceIndex, 1);
+//
+//   calculateTotal();
+//
+//   //remove the burger's name from the choicesArray
+//   var burgerName = $(event.target).siblings('.meat-name').html();
+//   var choiceIndex = choicesArray.indexOf(burgerName);
+//   choicesArray.splice(choiceIndex, 1);
+//
+//   makeDescription();
+//
+//   $('.choose-burger-button').show();
+// }
+//
+// function addBurger(event) {
+//   event.preventDefault();
+//
+//   var burgerPrice = Number($(event.target).parent().siblings('.burger-price').data("price").Price);
+//   var parent = $(event.target).parent().siblings('.burger-name');
+//   var meat = parent.html();
+//   var burgerId = parent.attr('id');
+//   var newBurger = $('<li></li>').html('<div class="meat-name">' + meat + '</div>');
+//   newBurger.attr('id', 'burger-' + burgerId);
+//   newBurger.attr('data-price', burgerPrice);
+//
+//   $('#order-list-burger').append(newBurger);
+//
+//   var deleteButton = $('<input>').html('X');
+//   deleteButton.attr('type', 'button');
+//   deleteButton.attr('onClick', 'removeBurger(event)');
+//
+//   newBurger.append(deleteButton);
+//
+//   $('.choose-burger-button').hide();
+//
+//   pricesArray.push(burgerPrice);
+//   calculateTotal();
+//
+//   choicesArray.push(meat);
+//   makeDescription();
+// }
+//
+// //choose a table dropdown
+// ///////////////////////////////////////////////////////////////////////////////////
+//
+// function chooseTable(){
+//   var tableNr = $('.dropdownmenu').val();
+//   $('#table-number').append(tableNr);
+// }
+//
+// ///////////////////////////////////////////////////////////////////////////////////
+// $(document).on('turbolinks:load', function(){
+//   $('.choose-burger-button').bind('click', addBurger);
+//
+//   $('.dropdownmenu').bind('change', chooseTable);
+//
+//   var check;
+//   $('.choose-topping-checkbox').bind('click', function(){
+//     check = $(event.target).prop("checked");
+//
+//     if(check) {
+//       console.log("checked");
+//       addTopping(event);
+//     } else {
+//       console.log("not checked");
+//       removeTopping(event);
+//     };
+//
+//   });
+//
+//   var check;
+//   $('.choose-side-checkbox').bind('click', function(){
+//     check = $(event.target).prop("checked");
+//
+//     if(check) {
+//       console.log("checked");
+//       addSide(event);
+//     } else {
+//       console.log("not checked");
+//       removeSide(event);
+//     };
+//
+//   });
+//
+//   var check;
+//   $('.choose-sauce-checkbox').bind('click', function(){
+//     check = $(event.target).prop("checked");
+//
+//     if(check) {
+//       console.log("checked");
+//       addSauce(event);
+//     } else {
+//       console.log("not checked");
+//       removeSauce(event);
+//     };
+//
+//   });
+//
+//   $('.place-order').bind('click', submitOrder);
+//
+//   $(function() {
+//     $('[data-toggle="popover"]').popover();
+//     container: 'body'
+//   });
+// });
+//
+// //popover
+// ///////////////////////////////////////////////////////////////////////////////////
--- app/assets/javascripts/hamburgers.js
+++ app/assets/javascripts/hamburgers.js
@@ -1,38 +1,38 @@
-
-// JS regarding admin pages ///////////
-
-function serveBurger(event){
-  event.preventDefault();
-  var id = $(event.target).data('id').Id;
-
-  $.ajax({
-    type: "PUT",
-    url: '/hamburgers/' + id,
-    data: JSON.stringify({
-      order: {
-        served: true
-      }
-    }),
-
-    contentType: "application/json",
-    dataType: "json"
-  })
-
-  .success(function() {
-    $("#serve-" + id).parent().remove();
-
-  })
-
-  .fail(function(error) {
-    errors = JSON.parse(error.responseText).error
-
-    $.each(errors, function(index, value) {
-      var errorItem = $("<li></li>").html(value);
-      $("#errors").append(errorItem);
-    });
-  });
-}
-
-$(document).ready(function() {
-  $('.serve-burger-button').bind('click', serveBurger);
-});
+// 
+// // JS regarding admin pages ///////////
+//
+// function serveBurger(event){
+//   event.preventDefault();
+//   var id = $(event.target).data('id').Id;
+//
+//   $.ajax({
+//     type: "PUT",
+//     url: '/hamburgers/' + id,
+//     data: JSON.stringify({
+//       order: {
+//         served: true
+//       }
+//     }),
+//
+//     contentType: "application/json",
+//     dataType: "json"
+//   })
+//
+//   .success(function() {
+//     $("#serve-" + id).parent().remove();
+//
+//   })
+//
+//   .fail(function(error) {
+//     errors = JSON.parse(error.responseText).error
+//
+//     $.each(errors, function(index, value) {
+//       var errorItem = $("<li></li>").html(value);
+//       $("#errors").append(errorItem);
+//     });
+//   });
+// }
+//
+// $(document).ready(function() {
+//   $('.serve-burger-button').bind('click', serveBurger);
+// });
